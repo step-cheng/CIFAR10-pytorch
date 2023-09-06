e227dd77ec878eb2e434fa06236cdbd356ea191f
Stephen Cheng <stephenscheng111@gmail.com>
2023-09-05 21:59:14 -0400

    added transfer vgg19, needs to be tested

---
+++ models/transfer.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+myVgg19 = models.vgg19_bn(weights=models.VGG19_BN_Weights.DEFAULT)
+
+for param in myVgg19.parameters():
+	param.requires_grad = False
+
+new_classifier = nn.Sequential(
+	nn.Linear(in_features=512, out_features=64, bias=True),
+	nn.ReLU(inplace=True),
+	nn.Linear(64,10,bias=True)
+)
+myVgg19.avgpool = nn.AvgPool2d(output_size=(1,1))
+myVgg19.classifier = new_classifier
