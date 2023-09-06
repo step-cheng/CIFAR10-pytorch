512a25419e0826b0845ad01ebcad2ac52c740c47
Stephen Cheng <stephenscheng111@gmail.com>
2023-09-05 23:58:38 -0400

    modified vgg transfer

--- models/vgg_transfer.py
+++ models/vgg_transfer.py
@@ -2,16 +2,19 @@ import torch
 import torch.nn as nn
 from torchvision import models
 
-
+# create an instance of Vgg model
 myVgg19 = models.vgg19_bn(weights=models.VGG19_BN_Weights.DEFAULT)
-
+myVgg19.avgpool = nn.Identity()
+myVgg19.features = myVgg19.features[:52]
 for param in myVgg19.parameters():
 	param.requires_grad = False
 
 new_classifier = nn.Sequential(
-	nn.Linear(in_features=512, out_features=64, bias=True),
+	nn.Linear(in_features=512*2*2, out_features=256, bias=True),
+	nn.BatchNorm1d(256),
 	nn.ReLU(inplace=True),
-	nn.Linear(64,10,bias=True)
+	nn.Linear(256,10,bias=True)
 )
-myVgg19.avgpool = nn.AvgPool2d(kernel_size=1,stride=1)
+
 myVgg19.classifier = new_classifier
+pass
