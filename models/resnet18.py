868e3d8a8d89b68b024db251556d290275f3a93a
Stephen Cheng <stephenscheng111@gmail.com>
2023-09-09 17:03:23 -0700

    idk what i changed, looks the same to me

--- models/resnet18.py
+++ models/resnet18.py
@@ -2,12 +2,9 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-print(models.resnet18())
 myResNet18 = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 
 for param in myResNet18.parameters():
 	param.requires_grad = False
 
 myResNet18.fc = nn.Linear(in_features=512,out_features=10, bias=True)
-
-print(myResNet18)
