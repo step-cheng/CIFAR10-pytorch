d7ab6bb6728134fd568e6d5f00becf292078736a
Stephen Cheng <stephenscheng111@gmail.com>
2023-09-12 10:13:28 -0700

    added 128 channel block, 85.1% accuracy

--- models/mymodel.py
+++ models/mymodel.py
@@ -31,20 +31,20 @@ class myNN(nn.Module):
             nn.Conv2d(64,64,kernel_size=3,padding=1,bias=False),
             nn.ReLU(),
             nn.MaxPool2d(2,stride=2),
-            nn.BatchNorm2d(64)
-            # nn.Conv2d(64,256,kernel_size=3,padding=1,bias=False),
-            # nn.ReLU(),
-            # nn.BatchNorm2d(256),
-            # nn.Conv2d(256,256,kernel_size=3,padding=1,bias=False),
-            # nn.ReLU(),
-            # nn.MaxPool2d(2,stride=2),
-            # nn.BatchNorm2d(256)
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64,128,kernel_size=3,padding=1,bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128,128,kernel_size=3,padding=1,bias=False),
+            nn.ReLU(),
+            nn.MaxPool2d(2,stride=2),
+            nn.BatchNorm2d(128)
             )
 
-    # input: Nx64x8x8    output: Nx10
+    # input: Nx128x4x4    output: Nx10
         self.linear_relu_stack = nn.Sequential(
             self.flat,           # Flatten class flattens starting at dimension default 1 and ending at dimension default -1 --> 16x2048
-            nn.Linear(64*8*8, 512, bias=True),
+            nn.Linear(128*4*4, 512, bias=True),
             nn.ReLU(),
             nn.Linear(512, 64, bias=True),
             nn.ReLU(),
